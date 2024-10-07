afa6e99299e0ddfb0727b0e8b79e40c1c6766dc2
DuyHai <duyha0207@gmail.com>
2024-10-07 10:40:47 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 
 import jakarta.servlet.DispatcherType;
 import vn.hoidanit.laptopshop.service.CustomUserDetailsService;
@@ -47,9 +49,18 @@ public class SecurityConfiguration {
         return new CustomSuccessHandler();
     }
 
+    @Bean
+    public SpringSessionRememberMeServices rememberMeServices() {
+            SpringSessionRememberMeServices rememberMeServices =
+                    new SpringSessionRememberMeServices();
+            // optionally customize
+            rememberMeServices.setAlwaysRemember(true);
+            return rememberMeServices;
+    }
+
 
     //v6. lamda
-     @Bean 
+    @Bean 
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception { 
         http 
                 .authorizeHttpRequests(authorize -> authorize 
@@ -63,6 +74,17 @@ public class SecurityConfiguration {
                 .hasRole("ADMIN")
 
                 .anyRequest().authenticated()) 
+
+                .sessionManagement((sessionManagement) -> sessionManagement
+                    .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                    .invalidSessionUrl("/logout?expired")
+                    .maximumSessions(1) // m đang dùng tk thì người khác kh đăng nhập tk đc nữa
+                    .maxSessionsPreventsLogin(false)) // người đăng nhập vào sẽ đá người đăng nhập trước đó ra
+
+                .logout(logout -> logout.deleteCookies("JSESSIONID").invalidateHttpSession(true))
+                // khi mà logout ra thì sẽ xóa bỏ cookies và thông báo với server là cái session đó đã hết hạn
+                .rememberMe(r -> r.rememberMeServices(rememberMeServices()))
+
                 //bay h cai login cua t thi m se vao cai url nay, neu fail thi nha ra error cho t
                 .formLogin(formLogin -> formLogin 
                         .loginPage("/login") 
